34386b5b0841ca96167c1521672aa584a7f87fa9
Deelite22 <jules.usfl@gmail.com>
2025-11-04 20:20:52 +0100

    Darstellung verbessert, Slab test hinzugefügt

--- ray.py
+++ ray.py
@@ -7,7 +7,7 @@ class Ray():
     Klasse die einen Strahl anhand Ursprung und Richtung beschreibt.
 
     Attribute:
-        direction, strahl: np.ndarray
+        direction, origin: np.ndarray
     """
     def __init__(self, origin: np.ndarray, direction: np.ndarray) -> None:
         if not ((origin.shape == (3,)) and (direction.shape == (3,))):
@@ -18,12 +18,10 @@ class Ray():
 
     @property
     def origin(self) -> np.ndarray:
-        """Get origin"""
         return self._origin
 
     @origin.setter
     def origin(self, value: np.ndarray):
-        """Set origin und validiere"""
         if not isinstance(value, np.ndarray):
             raise TypeError("Ursprung muss ein NumPy ndarray sein.")
         if value.shape != (3,):
@@ -32,30 +30,27 @@ class Ray():
 
     @property
     def direction(self) -> np.ndarray:
-        """Get direction"""
         return self._direction
 
     @direction.setter
     def direction(self, value: np.ndarray):
-        """Set, validiere und normalisiere direction"""
         if not isinstance(value, np.ndarray):
-            raise TypeError("direction muss ein NumPy ndarray sein")
+            raise TypeError("Richtung muss ein NumPy ndarray sein")
         if value.shape != (3,):
-            raise ValueError("direction muss ein 3D vector (shape (3,)) sein")
+            raise ValueError("Richtung muss ein 3D vector (shape (3,)) sein")
 
-        # Normalisiere den directionsvektor
+        # Normalisiere den Richtungsvektor
         norm = np.linalg.norm(value)
         if np.isclose(norm, 0):
-            raise ValueError("direction darf kein Nullvektor sein")
+            raise ValueError("Richtung darf kein Nullvektor sein")
         self._direction = value / norm
+        self._inverse_direction = (value / norm)**(-1) 
 
     def point_at_parameter(self, t: float) -> np.ndarray:
-        """
-        Gibt den Punkt entlang des Strahles nach t metern wieder
-        """
+        """Gibt den Punkt entlang des Strahles nach t metern wieder"""
         return self.origin + t * self.direction
 
-    def __repr__(self):
+    def __str__(self):
         return f"Ray( Ursprung={self.origin}, direction={self.direction})"
 
     def draw_ray(self, ax, t):
@@ -71,3 +66,28 @@ class Ray():
                 [start_point[1], end_point[1]], 
                 [start_point[2], end_point[2]], 
                 color='red', label=f'Ray (length={t})')
+        
+    def slab_test(self, box: copc.Box) -> bool:
+        t_min, t_max = 0.0, np.inf
+        box_min = np.array([box.x_min, box.y_min, box.z_min])
+        box_max = np.array([box.x_max, box.y_max, box.z_max])
+
+        #Für jede der drei Dimensionen, nicht vektorisiert
+        #for d in range(3):
+        #    t1 = (box_min[d] - self.origin[d]) * self._inverse_direction[d]
+        #    t2 = (box_max[d] - self.origin[d]) * self._inverse_direction[d]
+        #    t_min = max(t_min, min(t1, t2))
+        #    t_max = min(t_max, max(t1, t2))
+        
+        t1 = (box_min - self.origin) * self._inverse_direction
+        t2 = (box_max - self.origin) * self._inverse_direction
+
+        # minimaler und maximaler t-Wert pro Achse
+        # np.minimum/maximum vergleicht Elementweise
+        t_near = np.minimum(t1, t2)
+        t_far = np.maximum(t1, t2)
+
+        t_min = np.max(t_near) # spätest möglicher Eintrittszeitpunkt
+        t_max = np.min(t_far) # frühest möglicher Austrittszeitpunkt
+        
+        return (t_min < t_max)
