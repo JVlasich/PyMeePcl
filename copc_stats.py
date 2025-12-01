23baac50c72654c7897a45a32c10d1d3af9d41b0
Deelite22 <jules.usfl@gmail.com>
2025-12-01 16:22:34 +0100

    Skript verbessert, mehr optionen

--- copc_stats.py
+++ copc_stats.py
@@ -3,20 +3,29 @@ import argparse
 import statistics as stats
 import matplotlib.pyplot as plt
 from os import path
+import os
+import glob
 
 # Argument parser erstellen
 parser = argparse.ArgumentParser(description="Ein Script welches eine Statistik über die Nodes einer Copc Datei erstellt")
 parser.add_argument("-i", "--input", type=str, nargs="+", help="Pfad zu ein oder mehreren copc Datein: Wenn mehrere Dateien übergeben werden werden keine Histogramme erstellt")
-parser.add_argument("-o", "--output", dest="output", default="./statistics_summary.csv", type=str, help="Pfad zur csv File")
-parser.add_argument("-s", "--single_file", default=False, type=bool, help="write single file to csv")
+parser.add_argument("-o", "--output", default="./statistics_summary.csv", type=str, help="Pfad zur csv File")
+parser.add_argument("-wh", "--write_histogram", default=None, type=str, help="Speichert alle erstellten histogramme wenn ein Verzeichnis angegeben wird")
+parser.add_argument("-sh", "--show_histogram", action="store_true", help="Gibt ein Histogram am Bildschirm aus")
 args = parser.parse_args()
+
+# check ob input ein folder ist, funktioniert noch nicht wenn ordner nicht ./ ist
+if len(args.input) == 1:
+    if os.path.isdir(args.input[0]):
+        files = glob.glob("*.copc.laz", root_dir=args.input[0])
+        args.input = files
 
 readers, nodes_list, point_counts_lists, minimums, maximums, mediums, medians, stds = [], [], [], [], [], [], [], []
 for i, file in enumerate(args.input):
     readers.append(copc.FileReader(file))
     nodes_list.append(readers[i].GetAllNodes())
 
-    point_counts_lists.append([node.point_count for node in nodes_list[i]])
+    point_counts_lists.append([node.point_count for node in nodes_list[i] if node.point_count != 0])
 
     minimums.append(min(point_counts_lists[i]))
     maximums.append(max(point_counts_lists[i]))
@@ -24,48 +33,57 @@ for i, file in enumerate(args.input):
     medians.append(stats.median(point_counts_lists[i]))
     stds.append(stats.stdev(point_counts_lists[i]))
 
-if (len(args.input) == 1) and (not args.single_file):
-    text = f"""
-    Statistik:
-    Anzahl der Punkte:  {sum(point_counts_lists[0])}
-    Anzahl der Nodes:   {len(nodes_list[0])}
-    Maximale Tiefe:     {readers[0].GetMaxDepth()}
+if (len(args.input) == 1) or (args.write_histogram):
 
-    Minimum: {minimums[0]}
-    Maximum: {maximums[0]}
-    Minimum (ohne 0): {min([x for x in point_counts_lists[0] if x != 0])}
+    for i in range(len(args.input)):
+        text = f"""
+        Statistik:
+        Anzahl der Punkte:  {sum(point_counts_lists[i])}
+        Anzahl der Nodes:   {len(nodes_list[i])}
+        davon leere Nodes:  {len(nodes_list[i]) - len([x for x in point_counts_lists[i] if x != 0])}
 
-    Durchschnitt:   {round(mediums[0])}
-    Median:         {medians[0]}
+        Maximale Tiefe:     {readers[i].GetMaxDepth()}
 
-    Standardabweichung: {round(stds[0],1)}
-    """
-    print(text)
+        Minimum: {minimums[i]}
+        Maximum: {maximums[i]}
 
-    plt.figure(figsize=(14, 6))
-    plt.hist(point_counts_lists[0], bins=30, color='skyblue', edgecolor='black')
+        Durchschnitt:   {round(mediums[i])}
+        Median:         {medians[i]}
 
-    plt.text(1.02, 0.95, text, 
-            transform=plt.gca().transAxes, 
-            fontsize=10, 
-            verticalalignment='top', 
-            horizontalalignment='left',
-            fontfamily='monospace',
-            bbox=dict(boxstyle='round', facecolor='white', alpha=0.8)) # Weiße Box mit Transparenz
+        Standardabweichung: {round(stds[i],1)}
+        """
+        #print(text)
 
-    plt.subplots_adjust(right=0.7) 
+        plt.figure(figsize=(14, 6))
+        plt.hist(point_counts_lists[i], bins=30, color='skyblue', edgecolor='black')
 
-    plt.title(f'Histogram der Punktanzahlen\n{args.input[0]}')
-    plt.xlabel('Anzahl der Punkte')
-    plt.ylabel('Häufigkeit')
-    plt.grid()
-    plt.show()
+        plt.text(1.02, 0.95, text, 
+                transform=plt.gca().transAxes, 
+                fontsize=10, 
+                verticalalignment='top', 
+                horizontalalignment='left',
+                fontfamily='monospace',
+                bbox=dict(boxstyle='round', facecolor='white', alpha=0.8)) # Weiße Box mit Transparenz
 
-if args.single_file or (len(args.input) != 1):
+        plt.subplots_adjust(right=0.7) 
+
+        plt.title(f'Histogram der Punktanzahlen\n{args.input[i]}')
+        plt.xlabel('Anzahl der Punkte')
+        plt.ylabel('Häufigkeit')
+        plt.grid()
+
+        if args.write_histogram:
+            if not path.exists(args.write_histogram):
+                os.mkdir(args.write_histogram)
+            plt.savefig(f"{args.write_histogram}histogram_{args.input[i][2:]}.png")
+        if args.show_histogram:
+            plt.show()
+
+if args.output:
     if not path.exists(args.output):
         with open(args.output, "w") as ofile:
-            ofile.write("name,npoints,nnodes,max_depth,min,max,mean,median,std\n")
+            ofile.write("name,npoints,nnodes,nnodes_empty,max_depth,min,max,mean,median,std\n")
     
     with open(args.output, "a") as ofile:
         for i in range(len(args.input)):
-            ofile.write(f"{args.input[i]},{sum(point_counts_lists[i])},{len(nodes_list[i])},{readers[i].GetMaxDepth()},{minimums[i]},{maximums[i]},{round(mediums[i])},{medians[i]},{round(stds[i], 1)}\n")
+            ofile.write(f"{args.input[i]},{sum(point_counts_lists[i])},{len(nodes_list[i])},{len(nodes_list[i]) - len([x for x in point_counts_lists[i] if x != 0])},{readers[i].GetMaxDepth()},{minimums[i]},{maximums[i]},{round(mediums[i])},{medians[i]},{round(stds[i], 1)}\n")
